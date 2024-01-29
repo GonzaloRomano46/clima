23a71a2ac6c2cba83c0ccabcec9411057e0389aa
GonzaloRomano46 <131217885+GonzaloRomano46@users.noreply.github.com>
2024-01-28 22:43:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function getWeather() {
     const cityInput = document.getElementById('cityInput');
     const cityName = cityInput.value;
-
-    // Reemplaza 'TU_CLAVE_DE_API' con tu clave de API de OpenWeatherMap
     const apiKey = 'fd49804973dbfe837158979087490853';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
@@ -15,10 +13,29 @@ function getWeather() {
                 weatherInfo.innerHTML = `<p>No se encontró información para ${cityName}</p>`;
             } else {
                 const temperature = data.main.temp;
-                const description = data.weather[0].description;
+                const weatherDescription = translateWeather(data.weather[0].description);
 
-                weatherInfo.innerHTML = `<p>El clima en ${cityName} es ${description} con una temperatura de ${temperature}°C</p>`;
+                weatherInfo.innerHTML = `<p>El clima en ${cityName} es ${weatherDescription} con una temperatura de ${temperature}°C</p>`;
             }
         })
-        .catch(error => console.error('Error al obtener datos del clima:', error));
+        .catch(error => {
+            console.error('Error al obtener datos del clima:', error);
+            alert('Hubo un error al obtener los datos del clima. Por favor, intenta de nuevo más tarde.');
+        });
+}
+function translateWeather(description) {
+    const translations = {
+        'clear sky': 'cielo despejado',
+        'few clouds': 'pocas nubes',
+        'scattered clouds': 'nubes dispersas',
+        'broken clouds': 'nublado',
+        'shower rain': 'llovizna',
+        'rain': 'lluvia',
+        'thunderstorm': 'tormenta',
+        'snow': 'nieve',
+        'mist': 'niebla',
+        'overcast clouds': 'completamente nublado',
+    };
+
+    return translations[description] || description;
 }
